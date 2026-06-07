ea29c9fb72ec49041f81d682b130e798ba88856a
damccull <401104+damccull@users.noreply.github.com>
2026-06-06 22:31:35 -0500

    Adds ability to fully map out bounds for .text and .rdata sections

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum NmsFileReadError {
+    #[error("Invalid DOS header: {0}")]
+    InvalidDosHeader(&'static str),
+    #[error("Invalid PE header: {0}")]
+    InvalidPeHeader(&'static str),
+    #[error("Invalid COFFS header: {0}")]
+    InvalidCoffsHeader(&'static str),
+    #[error("Invalid or malformed optional header")]
+    InvalidOptionalHeader,
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
 use std::path::PathBuf;
 
-use anomaly_vector::{get_nms_mmap, get_pe_header_offset, get_section_offsets, telemetry};
-use tracing_subscriber::fmt::writer::MakeWriterExt;
+use anomaly_vector::{get_nms_mmap, get_pe_header_offset, read_pe_header, telemetry};
+use tracing::{debug, error, info};
 
 fn main() -> anyhow::Result<()> {
     //Configure log levels to channels
-    let stdout_sink = std::io::stdout.with_max_level(tracing::Level::INFO);
-    let stderr_sink = std::io::stderr.with_min_level(tracing::Level::WARN);
+    // let stdout_sink = std::io::stdout.with_min_level(tracing::Level::INFO);
+    // let stderr_sink = std::io::stderr.with_max_level(tracing::Level::WARN);
 
     telemetry::init_subscriber(
         "anomaly_vector".to_owned(),
         "info".to_owned(),
-        stdout_sink.and(stderr_sink),
+        std::io::stderr,
         None,
     );
+
+    ready_go()
+}
+
+#[tracing::instrument]
+fn ready_go() -> anyhow::Result<()> {
     println!("Hello, Traveller!");
     let path =
         PathBuf::from(format!("{}/.steam/steam/steamapps/common/No Man's Sky/Binaries/NMS.exe", std::env::var("HOME").unwrap()));
     if path.exists() {
-        println!("Found NMS.exe");
+        info!("Found NMS.exe");
     } else {
-        eprintln!("Can't find NMS.exe");
+        error!("Can't find NMS.exe");
     }
 
-    println!("Preparing memory map...");
+    info!("Preparing memory map...");
 
     let nms_mmap = get_nms_mmap(path)?;
 
-    println!("Memory map retrieved.");
+    info!("Memory map retrieved.");
     let pe_offset = get_pe_header_offset(&nms_mmap)?;
-    println!("PE header offset: {}", &pe_offset);
+    info!("PE header offset: {}", &pe_offset);
+
+    info!("Getting offsets for text and data sections...");
+    let pe_header_info = read_pe_header(&nms_mmap, pe_offset);
+
+    info!("Got PE Header Info");
+    debug!("PEHeaderInfo: {:#?}", &pe_header_info);
 
     Ok(())
 }
